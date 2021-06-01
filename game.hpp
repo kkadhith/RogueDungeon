eb9f2e0f719b7048fc716cbadb19e1fd6d0b08ef
nlee097 <nlee097@ucr.edu>
2021-05-31 19:33:53 -0700

    added basic combat controls and minor fixes

--- game.hpp
+++ game.hpp
@@ -12,6 +12,7 @@ using namespace std;
 #include "magician.hpp"
 #include "assassin.hpp"
 #include <string>
+#include "hostile.hpp"
 
 class Game
 {
@@ -20,6 +21,8 @@ private:
 	string input;
 	string userName;
 	int userRoleChoice;
+	vector<Hostile> hostiles;
+	Player* player;
 public:
 	Factory factory;
         Game() {};
@@ -34,6 +37,7 @@ public:
         	cout << "Quit Game      : [q]" << endl;
     	}	
     	void Begin() {
+		string userName;
         	cout << "Welcome to Generic Text RPG!" << endl;
         	cout << "Defeat enemies in each dungeon in order to level up and complete the game!" << endl;
         	cout << "Your progress will automatically be saved every dungeon you complete." << endl;
@@ -42,7 +46,7 @@ public:
 		factory.setName(userName);
 		cout << endl << "Please select a role:" << endl << "Warrior : [1]" << endl << "Archer : [2]" << endl << "Magician : [3]" << endl << "Assassin : [4]" << endl;
 		cin >> userRoleChoice;
-		Player* player = factory.selectRole(userRoleChoice);
+		player = factory.selectRole(userRoleChoice);
 		cout << endl;
     	}
     	void MainMenu() {
@@ -51,7 +55,52 @@ public:
         	// cout << "Save Game     : [s]" << endl;
         	cout << "Quit Game     : [q]" << endl;
     	}
-        	// void Fight();
+        void Fight() {
+		bool fin = false;
+		string input;
+		runChance = 60;		//player's chance at escaping from 0 to 100
+		cout << "You face off against a " << enemies.at(0).getDesc() << endl;
+		while(!fin) {
+			cout << "You have " << player.getHealth() << " health" << endl;
+			cout << "The " << enemies.at(0).getDesc() << "has " << enemies.at(0).getHealth() << " health" << endl;
+			cout << "Attack : [a]" << endl;
+			cout << "Defend : [d]" << endl;
+			cout << "Use item : [i]" << endl;
+			cout << "Special ability : [s]" << endl;
+			cout << "Run away : [r]" << endl;
+			cout << "Anything else to wait" << endl;
+			cin >> input;
+			if(input == "a"){
+				player.fight(enemies.at(0));
+			}else if(input == "d") {
+				//player.defend();
+			} else if(input == "i") {
+				//ShowInv();
+				//cin >> input;
+				//player.useItem(inventory.at(input));
+			} else if(input == "s") {
+				//player.abilities();
+			} else if(input == "r") {
+				int run = rand() % 100;
+			}
+			cout << "The " << enemies.at(0).getDesc() << "attacks you" << endl;
+			enemies.at(0).fightPlayer(player);
+			if(run < runChance) {
+				cout << "You got away!" << endl;
+				fin = true;
+			}
+			if(player.getHealth() <= 0) {
+				//lose
+				fin = true;
+			}
+			if(enemies.at(0).getHealth() <= 0) {
+				//win
+				player.updateEXP(/*enemyEXP*/0);
+				fin = true;
+			}
+		}
+		
+	}
         	bool IsPlaying() { return isPlaying; }
         	// void Shop();
         	// void EnterDungeon();
